ba9d47c2f27841f672b87fea5531940d8f048e6e
Li-yao Xia <lysxia@gmail.com>
2024-04-26 18:55:50 +0200

    Add tests for State

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,61 @@
+{-# LANGUAGE
+  BlockArguments,
+  ScopedTypeVariables,
+  TypeOperators #-}
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Bluefin.Eff (Eff, runPureEff, type (:>))
+import qualified Bluefin.State as B
+import Bluefin.Algae
+import Bluefin.Algae.State
+import Bluefin.Algae.Error
+import qualified Bluefin.Algae.Error.Cancellable as ErrorC
+import qualified Bluefin.Algae.NonDeterminism as NonDet
+import Bluefin.Algae.Coroutine
+
+-- * State
+
+-- Simple sanity test
+
+incr :: z :> zz => Handler (State Int) z -> Eff zz Int
+incr state = do
+  n <- get state
+  put state (n + 1)
+  pure n
+
+-- Distinguishing Bluefin.Algae.State (pure state) from Bluefin.State (IORef)
+
+algaeStateLitmus :: [Int]
+algaeStateLitmus = runPureEff $ NonDet.toList \choice ->
+  fst <$> (`runState` 0) \state -> do
+    _ <- NonDet.choose choice True False
+    incr state
+
+bluefinStateLitmus :: [Int]
+bluefinStateLitmus = runPureEff $ NonDet.toList \choice ->
+  B.evalState 0 \state -> do
+    _ <- NonDet.choose choice True False
+    incr' state
+  where
+    incr' state = do
+      n <- B.get state
+      B.put state (n + 1)
+      pure n
+
+testState :: TestTree
+testState = testGroup "State"
+  [ testCase "simple" $ runPureEff (runState incr 0) @?= (0, 1) 
+  , testCase "litmus-0" $ algaeStateLitmus @?= [0,0]
+  , testCase "litmus-1" $ bluefinStateLitmus @?= [0,1]
+  ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ testState
+  ]
