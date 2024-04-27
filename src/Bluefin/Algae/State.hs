c7e84f1fb34b221d265182f96789827e42fc8945
Li-yao Xia <lysxia@gmail.com>
2024-04-27 19:26:37 +0200

    Polish State

--- src/Bluefin/Algae/State.hs
+++ src/Bluefin/Algae/State.hs
@@ -12,38 +12,85 @@
 -- The 'runState' handler calls each continuation exactly once.
 -- It is compatible with single-shot continuations.
 module Bluefin.Algae.State
-  ( State(..)
+  ( -- * Operations
+    State(..)
   , get
   , put
+  , put'
+  , modify
+  , modify'
+
+    -- * Handlers
   , runState
+  , evalState
+  , execState
   ) where
 
 import Data.Kind (Type)
 import Bluefin.Eff (Eff, type (:&), type (:>))
 import Bluefin.Algae
 
-data State (s :: Type) (x :: Type) where
+-- | The state effect.
+data State (s :: Type) (r :: Type) where
+  -- | Get the current state.
   Get :: State s s
+  -- | Put a new state.
   Put :: s -> State s ()
 
+-- | Get the current state. Call the 'Get' operation.
 get :: z :> zz => Handler (State s) z -> Eff zz s
 get h = call h Get
 
+-- | Put a new state. Call the 'Put' operation.
 put :: z :> zz => Handler (State s) z -> s -> Eff zz ()
 put h s = call h (Put s)
 
-runState :: forall s a zz.
-  s ->
-  (forall z. Handler (State s) z -> Eff (z :& zz) a) ->
+-- | Strict variant of 'put'.
+put' :: z :> zz => Handler (State s) z -> s -> Eff zz ()
+put' h !s = call h (Put s)
+
+-- | Modify the state.
+modify :: z :> zz => Handler (State s) z -> (s -> s) -> Eff zz ()
+modify h f = get h >>= put h . f
+
+-- | Strict variant of 'modify'.
+modify' :: z :> zz => Handler (State s) z -> (s -> s) -> Eff zz ()
+modify' h f = get h >>= put' h . f
+
+-- | Pure state handler.
+runState ::
+  s ->  -- ^ Initial state
+  (forall z. Handler (State s) z -> Eff (z :& zz) a) ->  -- ^ Stateful computation
   Eff zz (a, s)
-runState s0 f = unwrap s0 (handle stateHandler (wrap . f))
+runState = runStateWith (,)
+
+-- | Return only the result value.
+evalState ::
+  s ->  -- ^ Initial state
+  (forall z. Handler (State s) z -> Eff (z :& zz) a) ->  -- ^ Stateful computation
+  Eff zz a
+evalState = runStateWith const
+
+-- | Return only the final state.
+execState ::
+  s ->  -- ^ Initial state
+  (forall z. Handler (State s) z -> Eff (z :& zz) a) ->  -- ^ Stateful computation
+  Eff zz s
+execState = runStateWith (const id)
+
+runStateWith :: forall s a r zz.
+  (a -> s -> r) ->  -- ^ Combine the result and final state.
+  s ->  -- ^ Initial state
+  (forall z. Handler (State s) z -> Eff (z :& zz) a) ->  -- ^ Stateful computation
+  Eff zz r
+runStateWith finish s0 f = unwrap s0 (handle stateHandler (wrap . f))
   where
-    stateHandler :: HandlerBody (State s) zz (s -> Eff zz (a, s))
+    stateHandler :: HandlerBody (State s) zz (s -> Eff zz r)
     stateHandler Get k = pure (\s -> k s >>= \k1 -> k1 s)
     stateHandler (Put s) k = pure (\_ -> k () >>= \k1 -> k1 s)
 
-    wrap :: Eff (z :& zz) a -> Eff (z :& zz) (s -> Eff zz (a, s))
-    wrap = fmap (\a s -> pure (a, s))
+    wrap :: Eff (z :& zz) a -> Eff (z :& zz) (s -> Eff zz r)
+    wrap = fmap (\a s -> pure (finish a s))
 
-    unwrap :: s -> Eff zz (s -> Eff zz b) -> Eff zz b
+    unwrap :: s -> Eff zz (s -> Eff zz r) -> Eff zz r
     unwrap s m = m >>= \k -> k s
