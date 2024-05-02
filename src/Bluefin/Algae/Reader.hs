1da8cedb42d2c79d60771cc90d07bd6e3bf28942
Li-yao Xia <lysxia@gmail.com>
2024-05-03 01:29:16 +0200

    Add Reader effect

---
+++ src/Bluefin/Algae/Reader.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE
+  DataKinds,
+  GADTs,
+  KindSignatures,
+  RankNTypes,
+  ScopedTypeVariables,
+  TypeOperators #-}
+-- | = Reader as an algebraic effect
+--
+-- Access to a read-only environment.
+module Bluefin.Algae.Reader
+  ( -- * Operation
+    Reader(..)
+  , ask
+
+    -- * Handler
+  , runReader
+  ) where
+
+import Data.Kind (Type)
+import Bluefin.Algae
+import Bluefin.Eff (Eff, type (:>), type (:&))
+
+-- | The reader effect.
+data Reader (a :: Type) (r :: Type) where
+  -- | Ask for a value.
+  Ask :: Reader a a
+
+-- | Ask for a value. Call the 'Ask' operation.
+ask :: s :> ss => Handler (Reader a) s -> Eff ss a
+ask h = call h Ask
+
+-- | Answer 'Ask' operations of the handled computation with a fixed value.
+runReader :: forall a b ss.
+  a -> (forall s. Handler (Reader a) s -> Eff (s :& ss) b) -> Eff ss b
+runReader a = handle readerHandler
+  where
+    readerHandler :: Reader a r -> (r -> Eff ss b) -> Eff ss b
+    readerHandler Ask k = k a
