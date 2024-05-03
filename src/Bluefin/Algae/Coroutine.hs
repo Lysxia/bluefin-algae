d95444acb12f70a9c21e56f69af979a87d582d92
Li-yao Xia <lysxia@gmail.com>
2024-05-03 18:22:06 +0200

    Sum of transducers

--- src/Bluefin/Algae/Coroutine.hs
+++ src/Bluefin/Algae/Coroutine.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE
   BangPatterns,
+  DataKinds,
   GADTs,
   KindSignatures,
   RankNTypes,
@@ -38,6 +39,8 @@ module Bluefin.Algae.Coroutine
   , CoTransducer
   , next
   , simpleTransducer
+  , voidTransducer
+  , eitherTransducer
 
     -- ** Pipes
   , Pipe(..)
@@ -46,6 +49,8 @@ module Bluefin.Algae.Coroutine
   , stepPipe
   , runPipe
   , forPipe
+  , eitherPipe
+  , loopPipe
 
     -- ** Handlers involving transducers and pipes
 
@@ -63,6 +68,7 @@ module Bluefin.Algae.Coroutine
   ) where
 
 import Data.Function (fix)
+import Data.Void (Void, absurd)
 import Bluefin.Eff
 import Bluefin.Algae
 
@@ -89,18 +95,35 @@ yield h o = call h (Yield o)
 -- |                  | <-(output o) |                    |
 -- +------------------+              +--------------------+
 -- @
-newtype Transducer o i m = MkTransducer (i -> CoTransducer o i m)
+newtype Transducer i o m = MkTransducer (i -> CoTransducer i o m)
 
 -- | Intermediate state of a 'Transducer' after receiving an input @i@.
-type CoTransducer o i m = m (o, Transducer o i m)
+type CoTransducer i o m = m (o, Transducer i o m)
 
 -- | Apply a transducer to yield the next output and transducer state.
-next :: Transducer o i m -> i -> m (o, Transducer o i m)
+next :: Transducer i o m -> i -> m (o, Transducer i o m)
 next (MkTransducer f) = f
 
 -- | A transducer which runs the same function on every input.
-simpleTransducer :: Functor m => (i -> m o) -> Transducer o i m
+simpleTransducer :: Functor m => (i -> m o) -> Transducer i o m
 simpleTransducer f = fix $ \self -> MkTransducer (\i -> (\o -> (o, self)) <$> f i)
+
+-- | Sum of transducers with the same output type, branching on the input type.
+eitherTransducer :: Functor m =>
+  (i -> Either i1 i2) ->   -- ^ Dispatch input
+  Transducer i1 o m ->     -- ^ Left transducer
+  Transducer i2 o m ->     -- ^ Right transducer
+  Transducer i o m
+eitherTransducer split = loop
+  where
+    loop t1 t2 = MkTransducer (transduce_ t1 t2 . split)
+    (<<&>>) = flip (fmap . fmap)
+    transduce_ t1 t2 (Left i1) = next t1 i1 <<&>> \t1' -> loop t1' t2
+    transduce_ t1 t2 (Right i2) = next t2 i2 <<&>> \t2' -> loop t1 t2'
+
+-- | Transducer with no input.
+voidTransducer :: Transducer Void o m
+voidTransducer = MkTransducer absurd
 
 -- * Pipes
 
@@ -132,7 +155,7 @@ stepPipe :: Pipe o i m a -> m (PipeEvent o i m a)
 stepPipe (MkPipe m) = m
 
 -- | Run a 'Pipe' with a transducer to respond to every event.
-runPipe :: Monad m => Transducer o i m -> Pipe i o m a -> m a
+runPipe :: Monad m => Transducer i o m -> Pipe i o m a -> m a
 runPipe t0 (MkPipe p) = p >>= loop t0
   where
     loop _ (Done a) = pure a
@@ -155,15 +178,37 @@ forPipe (MkPipe m) h = m >>= loop
     loop (Done a) = pure a
     loop (Yielding o k) = h o >>= \i -> k i >>= loop
 
+-- | Sum a transducer and a pipe with the same output type,
+-- branching on the input type.
+eitherPipe :: Monad m =>
+  (i -> Either i1 i2) ->   -- ^ Dispatch input
+  Transducer i1 o m ->     -- ^ Left input transducer
+  Pipe o i2 m a ->         -- ^ Right input pipe
+  Pipe o i m a
+eitherPipe split t0 (MkPipe p) = MkPipe (p >>= loop t0)
+  where
+    (<&>) = flip fmap
+    loop _ (Done a) = pure (Done a)
+    loop t (Yielding o k) = pure (Yielding o (switch t k . split))
+    switch t k (Left i1) = next t i1 <&> \(o, t') -> Yielding o (switch t' k . split)
+    switch t k (Right i2) = k i2 >>= loop t
+
+-- | Loop the output of a pipe back to its input.
+loopPipe :: Monad m => Pipe o o m a -> m a
+loopPipe (MkPipe p) = p >>= loop
+  where
+    loop (Done a) = pure a
+    loop (Yielding o k) = k o >>= loop
+
 -- * Handlers
 
 -- | Convert a coroutine that doesn't return into a 'Transducer'.
 toTransducer :: forall o i zz.
   (forall void. i -> ScopedEff (Coroutine o i) zz void) ->
-  Transducer o i (Eff zz)
+  Transducer i o (Eff zz)
 toTransducer f = MkTransducer (\o -> handle coroutineHandler (f o))
   where
-    coroutineHandler :: HandlerBody (Coroutine o i) zz (o, Transducer o i (Eff zz))
+    coroutineHandler :: HandlerBody (Coroutine o i) zz (o, Transducer i o (Eff zz))
     coroutineHandler (Yield i) k = pure (i, MkTransducer k)
 
 -- | Evaluate a coroutine into a 'Pipe'.
@@ -180,12 +225,12 @@ toPipe f = MkPipe (handle coroutineHandler (wrap . f))
 
 -- | Interleave the execution of a transducer and a coroutine.
 withTransducer :: forall o i a zz.
-  Transducer o i (Eff zz) ->
+  Transducer i o (Eff zz) ->
   ScopedEff (Coroutine i o) zz a ->
   Eff zz a
 withTransducer g f = with g (handle coroutineHandler (fmap wrap . f))
   where
-    coroutineHandler :: HandlerBody (Coroutine i o) zz (Transducer o i (Eff zz) -> Eff zz a)
+    coroutineHandler :: HandlerBody (Coroutine i o) zz (Transducer i o (Eff zz) -> Eff zz a)
     coroutineHandler (Yield o) k = pure $ \g1 -> do
       (i, g2) <- next g1 o
       with g2 (k i)
